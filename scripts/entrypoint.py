8f140c2a12a2ab9361e63a46b203c051ebc3bb89
omichaelis <38879457+oliviermichaelis@users.noreply.github.com>
2020-01-11 22:30:02 +0100

    moved dwd from rabbit to apiserver
    Signed-off-by: omichaelis <38879457+oliviermichaelis@users.noreply.github.com>

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -5,6 +5,8 @@ import pika
 import dataclasses
 import json
 import datetime
+import requests
+import sys
 
 
 @dataclasses.dataclass
@@ -14,6 +16,16 @@ class SensorValues:
     Temperature:    float
     Humidity:       float
     Pressure:       float
+
+
+class GoodEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            converted = dataclasses.asdict(o)
+            converted["Station"] = str(converted["Station"])
+            converted["Timestamp"] = str(converted["Timestamp"])
+            return converted
+        return super().default(o)
 
 
 def get_environment(variable: str, default: str) -> str:
@@ -52,7 +64,7 @@ def retrieve_data() -> list:
         time += delta
         result = client.query(station_id=433, timestamp=time)
 
-    print("Retrieved %d measurements", len(results))
+    print("Retrieved " + str(len(results)) + " measurements")
 
     return results
 
@@ -61,7 +73,6 @@ def parse_values(values: dict):
     if values is None:
         return None
 
-    # timestamp = datetime.datetime.strptime(str(body["datetime"]), "%Y%m%d%H%M")
     return SensorValues(Timestamp=str(values["datetime"]) + "00",    # this is needed for minutes
                         Station=values["station_id"],
                         Temperature=values["airtemp_temperature_200"],
@@ -69,45 +80,26 @@ def parse_values(values: dict):
                         Pressure=values["airtemp_pressure_station"])
 
 
-def publish(rabbit_channel, exchange: str, value: SensorValues):
-    if value is None:
+def transmit(measurement: SensorValues):
+    if not bool(measurement):
         return
 
-    class GoodEncoder(json.JSONEncoder):
-        def default(self, o):
-            if dataclasses.is_dataclass(o):
-                converted = dataclasses.asdict(o)
-                converted["Station"] = str(converted["Station"])
-                converted["Timestamp"] = str(converted["Timestamp"])
-                return converted
-            return super().default(o)
+    body_json = json.dumps(measurement, cls=GoodEncoder)
 
-    # body_json = json.dumps(dataclasses.asdict(value))
-    body_json = json.dumps(value, cls=GoodEncoder)
-    rabbit_channel.basic_publish(exchange=exchange, routing_key="sensor", body=body_json)
+    response = None
+    try:
+        response = requests.post("http://localhost:8080/measurements/climate", json=body_json)
+        if response.status_code is not 200:
+            print("Got http status code 200 as response")
+            sys.exit(2)
+    finally:
+        response.close()
 
 
 def main():
-    rabbit_url = get_environment("RABBITMQ_SERVICE_URL", "rabbitmq-ha.default.svc.cluster.local")
-    rabbit_port = int(get_environment("RABBITMQ_SERVICE_PORT", "5672"))
-    rabbit_queue = get_environment("RABBITMQ_QUEUE", "sensor")
-    rabbit_exchange = get_environment("RABBITMQ_EXCHANGE", "sensor")
-    rabbit_secret = get_environment("RABBITMQ_SECRET_PATH", "/credentials/rabbitmq")
-
-    parameters = pika.ConnectionParameters(host=rabbit_url,
-                                           port=rabbit_port,
-                                           credentials=retrieve_credentials(rabbit_secret))
-    connection = pika.BlockingConnection(parameters=parameters)
-    channel = connection.channel()
-
     for measurement in retrieve_data():
-        publish(channel, rabbit_exchange, parse_values(measurement))
-
-    connection.close()
+        transmit(parse_values(measurement))
 
 
 if __name__ == "__main__":
     main()
-
-    # for measurement in retrieve_data():
-    #     publish(None, "", parse_values(measurement))
